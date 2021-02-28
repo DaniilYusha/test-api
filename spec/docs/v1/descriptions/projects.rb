db407010e10a1a68abbd2a76b478ea5dc6b5028c
DaniilYusha <ilushenko79@gmail.com>
2021-02-28 14:30:17 +0200

    Add description for Projects section

---
+++ spec/docs/v1/descriptions/projects.rb
@@ -0,0 +1,22 @@
+module Docs
+  module V1
+    module Projects
+      extend Dox::DSL::Syntax
+
+      document :api do
+        resource 'Projects' do
+          endpoint '/projects'
+          group 'Projects'
+        end
+      end
+
+      document :index do
+        action 'Show all projects'
+      end
+
+      document :create do
+        action 'Create a project'
+      end
+    end
+  end
+end
